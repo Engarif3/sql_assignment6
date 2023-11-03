27b5b7bf944fe06e581a759f457914bbedaf28a3
Md Arifur Rahman <arif.aust.eng@gmail.com>
2023-11-03 13:57:12 +0100

    assignment 6 done

---
+++ Task3.sql
@@ -0,0 +1,6 @@
+SELECT C.name AS category_name, SUM(OI.quantity * OI.unit_price) AS total_revenue
+FROM Categories C
+JOIN Products P ON C.category_id = P.category_id
+JOIN Order_Items OI ON P.product_id = OI.product_id
+GROUP BY C.category_id, C.name
+ORDER BY total_revenue DESC;
